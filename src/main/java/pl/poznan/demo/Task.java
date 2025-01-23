e6187bb3a302d59eda8a25dc1eae98d328934050
Eteer Eteer <p.komorowski1998@gmail.com>
2025-01-23 20:37:45 +0100

    Poprawienie klasy TASK.JAVA z uzyciem LOMBOKA, dodanie TaskRepository

--- src/main/java/pl/poznan/demo/Task.java
+++ src/main/java/pl/poznan/demo/Task.java
@@ -6,10 +6,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 public class Task {
 
     @Id
@@ -21,35 +26,17 @@ public class Task {
     @Enumerated(EnumType.STRING)
     private TaskStatus status;
 
-    protected Task() {
+    private Integer priority;
+    private LocalDate dueDate;
+
+    // Konstruktor bezargumentowy wymagany przez JPA
+    public Task() {
     }
 
-    // Błędem który zobaczyłem jest użycie "status" jako "this.status" problem, że on nie będzie miał nigdy miejsca, zamieniłem je miejscami
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        // tutaj linia była zakomentowana wydaje mi się że to był zabieg celowy lecz postanowiłem o tym wspomnieć w komentarzu
+    // Konstruktor umożliwiający inicjalizację zadania z danymi
+    public Task(String description, TaskStatus status) {
         this.description = description;
-    }
-
-
-    // Tutaj metoda była prywatna i zostało to zmienione na Publiczne ponieważ otrzymanie statusu możemy rządać z zewnątrz a nie koniecznie tylko z samej klasy Task.
-    public TaskStatus getStatus() {
-        return status;
+        this.status = status;
     }
 
     @Override
@@ -59,10 +46,22 @@ public class Task {
         return Objects.equals(id, task.id) && Objects.equals(description, task.description) && status == task.status;
     }
 
-    // hashCode nie powinno zawsze zwracać 0. Bo nie ma wtedy sensu stosowania tej metody, ponieważ wtedy wszystkie wartości są sobie równe po porównaniu na podstawie hashCode.
-    // Wykorzystałem 3 najważniejsze dane do stworzenia hashCode czyli Id, Opis i status
+    // Hashcode nie powinno zawsze zwracać 0. Bo nie ma wtedy sensu stosowania tej metody, ponieważ wtedy wszystkie wartości są sobie równe po porównaniu na podstawie hashcode.
+    // Wykorzystałem 3 najważniejsze dane do stworzenia hashcode, czyli Id, Opis i status
     @Override
     public int hashCode() {
         return Objects.hash(id, description, status);
     }
 }
+
+
+/*
+ * Na obecną chwilę mogę wspomnieć o poprawkach, jakie miały miejsce w metodach:
+ * - SetStatus,
+ * - SetDescription,
+ * - prywatnej metodzie getStatus.
+ *
+ * Nie mogę już jak we wcześniejszym commicie wskazać miejsc tych poprawek, gdyż zastosowałem LOMBOKA w celu uproszczenia kodu,
+ * jak wymaga tego zadanie. Użycie Lomboka uznałem za najlepsze rozwiązanie, aby zachować czystość i czytelność kodu,
+ * jednocześnie nie zaśmiecając klasy zbędnymi metodami getterami i setterami.
+ */
